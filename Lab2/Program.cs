2fa8c5e7c3b836edfde341fca70b4af2507c761e
Lei Sun <139188566@qq.com>
2023-01-25 17:05:31 -0600

    M

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -2,41 +2,49 @@
 
 CarPark carPark = new CarPark(20);
 
-//Add vehicle
-carPark.ParkVehicle("A00001");
-carPark.ParkVehicle("A00002");
-carPark.ParkVehicle("A00003");
+try 
+{ 
+    //Add vehicle
+    carPark.ParkVehicle("A00001555");
+    carPark.ParkVehicle("A00002");
+    carPark.ParkVehicle("A00003");
 
-foreach (var parkingSpot in carPark.hsParkingSpot)
-{
-    if (parkingSpot.SpotVehicle != null)
+
+    foreach (var parkingSpot in carPark.hsParkingSpot)
     {
-        Console.WriteLine($"Spot{parkingSpot.SpotNumber} has vehicle {parkingSpot.SpotVehicle.LicenseNumber}");
+        if (parkingSpot.SpotVehicle != null)
+        {
+            Console.WriteLine($"Spot{parkingSpot.SpotNumber} has vehicle {parkingSpot.SpotVehicle.LicenseNumber}");
        
-    }
-    else
-    {
-        Console.WriteLine($"Spot{parkingSpot.SpotNumber}  has  no vehicle !");
+        }
+        else
+        {
+            Console.WriteLine($"Spot{parkingSpot.SpotNumber}  has  no vehicle !");
+
+        }
 
     }
-
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
 }
 
 Console.WriteLine("-------------------------");
 
-//Remove  vehicle
-carPark.RemoveVehicle("A00001");
-foreach (var parkingSpot in carPark.hsParkingSpot)
-{
-    if (parkingSpot.SpotVehicle != null)
-    {
-        Console.WriteLine($"Spot{parkingSpot.SpotNumber} has vehicle {parkingSpot.SpotVehicle.LicenseNumber}");
+////Remove  vehicle
+//carPark.RemoveVehicle("A00001");
+//foreach (var parkingSpot in carPark.hsParkingSpot)
+//{
+//    if (parkingSpot.SpotVehicle != null)
+//    {
+//        Console.WriteLine($"Spot{parkingSpot.SpotNumber} has vehicle {parkingSpot.SpotVehicle.LicenseNumber}");
 
-    }
-    else
-    {
-        Console.WriteLine($"Spot{parkingSpot.SpotNumber}  has  no vehicle !");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"Spot{parkingSpot.SpotNumber}  has  no vehicle !");
 
-    }
+//    }
 
-}
+//}
