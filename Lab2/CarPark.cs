3bf62c84f60c35b99c16ef932154f0cd7b91204d
Lei Sun <139188566@qq.com>
2023-01-25 17:01:34 -0600

    Create lab2

---
+++ Lab2/CarPark.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class CarPark
+    {
+        public HashSet<ParkingSpot> hsParkingSpot =new HashSet<ParkingSpot>();
+
+        public CarPark(int capacity)
+        {
+            for(int i = 1; i <= capacity; i++)
+            {
+                ParkingSpot parkingSpot = new ParkingSpot(i);
+                parkingSpot.SpotCarPark = this;
+                hsParkingSpot.Add(parkingSpot);
+            }
+            
+        }
+
+        public void ParkVehicle(string licenseNumber)
+        {
+
+            foreach(var parkingSpot in hsParkingSpot)
+            {
+               if( parkingSpot.SpotVehicle ==null)
+                {
+                    Vehicle vehicle = new Vehicle(licenseNumber);
+                    vehicle.VehicleParkingSpot = parkingSpot;
+                    parkingSpot.SpotVehicle = vehicle;
+
+                    break;
+                    
+                }
+            }
+
+        }
+        public void RemoveVehicle(string licenseNumber)
+        {
+            foreach (var parkingSpot in hsParkingSpot)
+            { 
+                if(parkingSpot.SpotVehicle.LicenseNumber== licenseNumber)
+                {
+                    parkingSpot.SpotVehicle.VehicleParkingSpot = null;
+                    parkingSpot.SpotVehicle = null;
+                    break;
+                }
+            }
+         }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,42 @@
+﻿using Lab2;
+
+CarPark carPark = new CarPark(20);
+
+//Add vehicle
+carPark.ParkVehicle("A00001");
+carPark.ParkVehicle("A00002");
+carPark.ParkVehicle("A00003");
+
+foreach (var parkingSpot in carPark.hsParkingSpot)
+{
+    if (parkingSpot.SpotVehicle != null)
+    {
+        Console.WriteLine($"Spot{parkingSpot.SpotNumber} has vehicle {parkingSpot.SpotVehicle.LicenseNumber}");
+       
+    }
+    else
+    {
+        Console.WriteLine($"Spot{parkingSpot.SpotNumber}  has  no vehicle !");
+
+    }
+
+}
+
+Console.WriteLine("-------------------------");
+
+//Remove  vehicle
+carPark.RemoveVehicle("A00001");
+foreach (var parkingSpot in carPark.hsParkingSpot)
+{
+    if (parkingSpot.SpotVehicle != null)
+    {
+        Console.WriteLine($"Spot{parkingSpot.SpotNumber} has vehicle {parkingSpot.SpotVehicle.LicenseNumber}");
+
+    }
+    else
+    {
+        Console.WriteLine($"Spot{parkingSpot.SpotNumber}  has  no vehicle !");
+
+    }
+
+}
